ed5f5dd29fe4969e5f6e8b4c003afb0a1a062761
Kirby Banman <kdbanman@ualberta.ca>
2016-02-29 22:19:06 -0700

    get rid of dummy implementation.  project is now ready for actual development.

--- polyball/shared/model.js
+++ polyball/shared/model.js
@@ -1,33 +1,7 @@
-// Animation frame polyfill - caps callbacks at ~62.5 fps (16ms interval).
-(function() {
-    var lastTime = 0;
-    GLOBAL.window = {}
-    GLOBAL.window.requestAnimationFrame = function(callback, element) {
-        var currTime = new Date().getTime();
-        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-        var id = setTimeout(function() {
-            callback(currTime + timeToCall);
-            }, timeToCall);
-        lastTime = currTime + timeToCall;
-        return id;
-    };
- 
-    GLOBAL.window.cancelAnimationFrame = function(id) {
-        clearTimeout(id);
-    };
-}());
 
-var Physics = require('bower_components/PhysicsJS/dist/physicsjs-full.js');
-Physics(function(world){
+var Model = function () {
 
-  // subscribe to ticker to advance the simulation
-  Physics.util.ticker.on(function(time){
-      world.step(time);
-  });
+};
 
-  // start the ticker
-  Physics.util.ticker.start();
-
-});
-
+exports = Model;
 
