856e4b0e503c8e07c3023bb69f1084dde2df2d03
Jared Rewerts <rewerts@ualberta.ca>
2016-03-07 16:23:13 -0700

    Commented Paddle more.

--- polyball/shared/model/Paddle.js
+++ polyball/shared/model/Paddle.js
@@ -30,7 +30,21 @@ var Paddle = function(config) {
     );
 };
 
+/**
+ * Updates the position of the paddle to the specified value.
+ * The position is forced inside the bounds of the player's goal,
+ * so that the paddle doesn't leave or clip with the goal.
+ * @param x: number
+ */
 Paddle.prototype.setPosition = function(x) {
+    // The reason we involve size is to account for the width of the paddle.
+    if (this.leftBound.x > (x - this.size / 2)) {
+        x = this.leftBound + this.size / 2;
+    }
+    else if ( this.rightBound.x < (x + this.size / 2)) {
+        x = this.rightBound - this.size / 2;
+    }
+
     this.body.x = x;
 };
 
