c093789a4566018031a17c3946c9667fddaf85f8
kdbanman <kirby.banman@gmail.com>
2016-03-07 17:07:50 -0700

    start model skeleton.

--- polyball/shared/Model.js
+++ polyball/shared/Model.js
@@ -1,6 +1,28 @@
+"use strict"
+var Physics = require('physicsjs');
 
-var Model = function () {
+var Ball = require('polyball/model/Ball');
+var Paddle = require('polyball/model/Paddle');
+var Player = require('polyball/model/Player');
 
+
+/**
+ *
+ * @param {{
+ *
+ * }} config
+ * @constructor
+ */
+var Model = function (config) {
+    this.players = [];
+    this.spectators = [];
+    this.playerQueue = [];
+
+    this.world = Physics();
+
+    this.balls = [];
+    this.powerups = [];
+    this.election = undefined;
 };
 
 module.exports = Model;
