05b76897499bd99736b3f406b9f3571812178fdb
Jared Rewerts <rewerts@ualberta.ca>
2016-03-08 12:46:45 -0700

    Added an allowance for floating point calculations.

--- polyball/tests/shared/model/ArenaTest.js
+++ polyball/tests/shared/model/ArenaTest.js
@@ -4,6 +4,9 @@ var Arena = require('polyball/shared/model/Arena.js');
 
 describe('Arena', function() {
     "use strict";
+
+    var fpDelta = 0.001;
+
     describe('#getCoordinates(theta, radius, midX, midY)', function() {
         it('should match the unit circles cardinal values', function() {
             var test0 = Arena.getCoordinates(0, 1, 0, 0);
@@ -12,20 +15,20 @@ describe('Arena', function() {
             var test3 = Arena.getCoordinates(Math.PI*3/2, 1, 0, 0);
             var test4 = Arena.getCoordinates(Math.PI*2, 1, 0, 0);
 
-            test0.x.should.equal(1);
-            test0.y.should.equal(0);
+            test0.x.should.approximately(1, fpDelta);
+            test0.y.should.approximately(0, fpDelta);
 
-            test1.x.should.equal(0);
-            test1.y.should.equal(1);
+            test1.x.should.approximately(0, fpDelta);
+            test1.y.should.approximately(1, fpDelta);
 
-            test2.x.should.equal(-1);
-            test2.y.should.equal(0);
+            test2.x.should.approximately(-1, fpDelta);
+            test2.y.should.approximately(0, fpDelta);
 
-            test3.x.should.equal(0);
-            test3.y.should.equal(-1);
+            test3.x.should.approximately(0, fpDelta);
+            test3.y.should.approximately(-1, fpDelta);
 
-            test4.x.should.equal(1);
-            test4.y.should.equal(0);
+            test4.x.should.approximately(1, fpDelta);
+            test4.y.should.approximately(0, fpDelta);
         });
     });
 });
