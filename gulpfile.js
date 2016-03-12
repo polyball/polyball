6c39f27700af62705a9dbe88be4127655c4fafa8
Kirby Banman <kdbanman@ualberta.ca>
2016-03-11 18:45:01 -0700

    try AGAIN to make travis glow red when browserify fails.

--- gulpfile.js
+++ gulpfile.js
@@ -49,8 +49,10 @@ gulp.task('watch-js', watchifyBundle);
 function bundle(bundler) {
     return bundler.bundle()
         // log errors if they happen
-        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-        .on('error', function(){this.emit('end');} )
+        //.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .on('error', function(err){
+            console.log(err);
+            this.emit('end');})
         .pipe(source('client-bundle.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
