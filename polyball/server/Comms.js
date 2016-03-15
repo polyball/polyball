b32d4218d5627f57f3914c2adc9aff500e274efb
kdbanman <kirby.banman@gmail.com>
2016-03-14 18:18:39 -0600

    start to define how the Comms module will look

--- polyball/server/Comms.js
+++ polyball/server/Comms.js
@@ -1,3 +1,56 @@
 /**
  * Created by kdbanman on 2/29/16.
  */
+
+
+var socket = require('socket.io');
+var Util = require('polyball/shared/Util');
+var Client = require('polyball/shared/model/Client');
+
+
+/**
+ *
+ * @param {{
+ *   httpServer: Object,
+ *   model: Model
+ * }}config
+ * @constructor
+ */
+var Comms = function (config) {
+    var io = socket(config.httpServer);
+    var model = config.model;
+
+    io.on('connection', function (clientSocket) {
+
+        clientSocket.on('disconnect', function () {
+
+        });
+
+        clientSocket.on('vote', function () {
+            //broadcast to listeners (like the engine).
+            //listeners.notify('vote', voteData);
+        });
+
+        // create a spectator with the socket in the model
+        var client = new Client({
+            name: Util.randomUsername(),
+            socket: clientSocket
+            });
+
+        model.addSpectator(client);
+    });
+
+
+
+    this.broadcastSnapshot = function () {
+        // get spectators (get their clients)
+        // get players (same)
+
+        // for each client, socket.emit('newSnapshot', snapshot);
+
+    };
+
+
+};
+
+module.export = Comms;
