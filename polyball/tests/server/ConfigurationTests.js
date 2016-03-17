9887f9d0b307b6b8676caa80d69ff07ef56488cf
ryant26 <ryant26@gmail.com>
2016-03-16 21:27:49 -0600

    Added constraints to configuration: - min players must be > 3 - round intermission must be > 0 - Added tests as well
    Closes #94

--- polyball/tests/server/ConfigurationTests.js
+++ polyball/tests/server/ConfigurationTests.js
@@ -4,9 +4,14 @@
 var Configuration = require('polyball/server/Configuration');
 var should = require('should');
 var Util = require('polyball/shared/Util');
+var Logger = require('polyball/shared/Logger');
+
 
 describe('Configuration', function () {
     describe('#Construct', function () {
+        before(function (){
+            Logger.setLevel('ERROR');
+        });
         it('should read in a json file', function(){
             var path = Util.createJSONConfigFile({hello: 1, goodbye:2});
             var config = new Configuration({configPath: path});
@@ -25,6 +30,21 @@ describe('Configuration', function () {
             var path = Util.createConfigFile('{minimumPlayers: 10:gsfg:. .. serverTick: 1}');
             should.throws(function (){new Configuration({configPath: path});});
             Util.deleteJSONConfigFile(path);
+        });
+        it('should not allow min players < 3', function(){
+            var path = Util.createJSONConfigFile({minimumPlayers: 2});
+            var config = new Configuration({configPath: path});
+            config.values.minimumPlayers.should.equal(3);
+            Util.deleteJSONConfigFile(path);
+        });
+        it('should not allow round intermission < 1', function(){
+            var path = Util.createJSONConfigFile({roundIntermission: 0});
+            var config = new Configuration({configPath: path});
+            config.values.roundIntermission.should.equal(5000);
+            Util.deleteJSONConfigFile(path);
+        });
+        after(function(){
+            Logger.setLevel('INFO');
         });
     });
 });
