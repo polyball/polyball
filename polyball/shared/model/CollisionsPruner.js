6d2c677404de9db66d32be4c9a8b63b6d71a1add
Kirby Banman <kdbanman@ualberta.ca>
2016-03-24 15:02:05 -0600

    document and test collisions pruner.  closes #146.

--- polyball/shared/model/CollisionsPruner.js
+++ polyball/shared/model/CollisionsPruner.js
@@ -5,6 +5,20 @@
 var Physics = require('physicsjs');
 var lodash = require('lodash');
 
+/**
+ *
+ * Splits and retransmits physics world collisions:detected events on one of two channels:
+ *
+ * - impulseCollisions:detected for the collisions that do not involve an ignored body
+ * - nonimpulseCollisions:detected for the collisions that do involve an ignored body
+ *
+ * The above events are rebroadcast through the same world that CollisionsPruner was constructed with.
+ * The world is also given impulse response, collision detection, and sweep prune behaviours.
+ *
+ * @param {Object} config
+ * @property {Physics.world} config.world - the world whose collision events will be retransmitted.
+ * @constructor
+ */
 var CollisionsPruner = function (config) {
     var world = config.world;
     var ignoredBodies = [];
@@ -26,7 +40,7 @@ var CollisionsPruner = function (config) {
         // extract collisions that involve ignored bodies
         nonImpulseEvent.collisions = lodash.remove(event.collisions, function (collision) {
 
-            // iterate throuh ignored bodies, returning one that is in the collision (if any)
+            // iterate through ignored bodies, returning one that is in the collision (if any)
             var ignoredBodyFromCollision = lodash.find(ignoredBodies, function (ignoredBody) {
                 return ignoredBody === collision.bodyA || ignoredBody === collision.bodyB;
             });
@@ -39,12 +53,20 @@ var CollisionsPruner = function (config) {
         world.emit('impulseCollisions:detected', event);
     });
 
+    /**
+     * Adds a body to the ignore list - body will not be given impulse response.
+     * @param body
+     */
     this.addIgnoredBody = function (body) {
         if (body != null) {
             ignoredBodies.push(body);
         }
     };
 
+    /**
+     * Removes a body from the ignore list - body will now be given impulse response.
+     * @param body
+     */
     this.removeIgnoredBody = function (body) {
         if (body != null) {
             lodash.remove(ignoredBodies, function (ignoredBody) { return body === ignoredBody;});
