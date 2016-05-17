3696925d48e55ee25de842118b9acf7409292f3e
ryant26 <ryant26@gmail.com>
2016-05-16 21:24:37 -0600

    Moved ID Generator code from model into helper class

--- polyball/shared/utilities/IdGenerator.js
+++ polyball/shared/utilities/IdGenerator.js
@@ -1,3 +1,19 @@
 /**
  * Created by ryan on 16/05/16.
  */
+var IdGenerator = {
+    nextID : (function () {
+        var nextID = 1;
+
+        return function () {
+
+            if (typeof window !== 'undefined') {
+                throw new Error("Client Model must not create its own IDs!");
+            }
+
+            return nextID++;
+        };
+    }())
+};
+
+module.exports = IdGenerator;
