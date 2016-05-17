d613a0c5abeb863bd7f84c227f10cf8da175e9f3
ryant26 <ryant26@gmail.com>
2016-05-17 17:37:39 -0600

    Added Ball Container

--- polyball/shared/Model.js
+++ polyball/shared/Model.js
@@ -3,12 +3,9 @@
 var _ = require('lodash');
 var Physics = require('physicsjs');
 var Logger = require('polyball/shared/Logger');
-var Arena = require('polyball/shared/model/Arena');
-var Ball = require('polyball/shared/model/Ball');
 var Spectator = require('polyball/shared/model/Spectator');
 var Player = require('polyball/shared/model/Player');
 var Util = require('polyball/shared/utilities/Util');
-var PowerupFactory = require('polyball/shared/powerups/PowerupFactory'); // jshint ignore:line
 var EngineStatus = require('polyball/shared/EngineStatus.js');
 var PowerupElection = require('polyball/shared/powerups/PowerupElection');
 
@@ -94,8 +91,6 @@ var Model = function () {
     this.gameStatus = EngineStatus.gameInitializing;
     this.collisionsPruner = null;
 
-
-
     /**
      * @returns {World}
      */
@@ -153,92 +148,6 @@ var Model = function () {
         localClientID = newID;
     };
 
-    //
-    //             BALLS
-    //
-    ///////////////////////////////////////////////////////////////////////////
-
-    /**
-     * Add a ball to the model.
-     *
-     * @param {Object} config - see Ball constructor
-     * @property {Number} [config.id] - Optional.  Should not be passed on the server, should always be passed on the client.
-     * @return {Ball} The new Ball.
-     */
-    this.addBall  = function (config) {
-
-        var newConfig = {
-            id: config.id ? config.id : nextID(),
-        };
-
-        _.assign(newConfig, config);
-
-        var ball = new Ball(newConfig);
-        balls.push(ball);
-
-        world.addBody(ball.body);
-
-        return ball;
-    };
-
-
-
-    /**
-     * @param {Number|Predicate} id - Either the ID of the Ball, or a boolean returning callback that takes a Ball.
-     * @return {Ball} Ball identified by id.
-     */
-    this.getBall = function (id) {
-        return findSingle(balls, id);
-    };
-
-    /**
-     * Get all balls satisfying the predicate callback.
-     * @param {Predicate} [predicate]  Callback to evaluate for each ball. (matches all if null.)
-     * @returns {Ball[]} All balls matching the predicate. (may be empty.)
-     */
-    this.getBalls = function (predicate) {
-        return findAll(balls, predicate);
-    };
-
-    /**
-     * @param {Number|Predicate} id - Either the ID of the Ball, or a boolean returning callback that takes a Ball.
-     * @returns {boolean} True iff the model has the ball identified by id.
-     */
-    this.hasBall = function (id) {
-        return this.getBall(id) != null;
-    };
-
-    /**
-     * @returns {Number} The number of balls in the model.
-     */
-    this.ballCount = function () {
-        return balls.length;
-    };
-
-    /**
-     * Delete the identified ball from the model.
-     *
-     * @param {Number} id
-     */
-    this.deleteBall = function (id) {
-        var ball = removeByID(balls, id);
-
-        if (ball != null) {
-            world.removeBody(ball.body);
-        }
-    };
-
-    /**
-     * Delete all balls from the model.
-     */
-    this.clearBalls = function () {
-        var ballIDs = _.map(balls, function (ball) { return ball.id; });
-
-        var me = this;
-        ballIDs.forEach(function (id) {
-            me.deleteBall(id);
-        });
-    };
 
     //
     //             SPECTATORS
---
+++ polyball/shared/model/Containers/BallContainer.js
@@ -0,0 +1,102 @@
+/**
+ * Created by ryan on 17/05/16.
+ */
+
+var _ = require('lodash');
+var ArrayHelper = require('polyball/shared/utilities/ArrayHelper');
+var IdGenerator = require('polyball/shared/utilities/IdGenerator');
+var Ball = require('polyball/shared/model/Ball');
+
+/**
+ * Initializes the PowerupContainer
+ * @property {world} Physics.world
+ * @constructor
+ */
+var BallContainer = function (config) {
+    var balls = [];
+    var world = config.world;
+    var IdGen = new IdGenerator();
+
+    /**
+     * Add a ball to the model.
+     *
+     * @param {Object} config - see Ball constructor
+     * @property {Number} [config.id] - Optional.  Should not be passed on the server, should always be passed on the client.
+     * @return {Ball} The new Ball.
+     */
+    this.addBall  = function (config) {
+
+        var newConfig = {
+            id: config.id ? config.id : IdGen.nextID(),
+        };
+
+        _.assign(newConfig, config);
+
+        var ball = new Ball(newConfig);
+        balls.push(ball);
+
+        world.addBody(ball.body);
+
+        return ball;
+    };
+
+    /**
+     * @param {Number|Predicate} id - Either the ID of the Ball, or a boolean returning callback that takes a Ball.
+     * @return {Ball} Ball identified by id.
+     */
+    this.getBall = function (id) {
+        return ArrayHelper.findSingle(balls, id);
+    };
+
+    /**
+     * Get all balls satisfying the predicate callback.
+     * @param {Predicate} [predicate]  Callback to evaluate for each ball. (matches all if null.)
+     * @returns {Ball[]} All balls matching the predicate. (may be empty.)
+     */
+    this.getBalls = function (predicate) {
+        return ArrayHelper.findAll(balls, predicate);
+    };
+
+    /**
+     * @param {Number|Predicate} id - Either the ID of the Ball, or a boolean returning callback that takes a Ball.
+     * @returns {boolean} True iff the model has the ball identified by id.
+     */
+    this.hasBall = function (id) {
+        return this.getBall(id) != null;
+    };
+
+    /**
+     * @returns {Number} The number of balls in the model.
+     */
+    this.ballCount = function () {
+        return balls.length;
+    };
+
+    /**
+     * Delete the identified ball from the model.
+     *
+     * @param {Number} id
+     */
+    this.deleteBall = function (id) {
+        var ball = ArrayHelper.removeByID(balls, id);
+
+        if (ball != null) {
+            world.removeBody(ball.body);
+        }
+    };
+
+    /**
+     * Delete all balls from the model.
+     */
+    this.clearBalls = function () {
+        var ballIDs = _.map(balls, function (ball) { return ball.id; });
+
+        var me = this;
+        ballIDs.forEach(function (id) {
+            me.deleteBall(id);
+        });
+    };
+
+};
+
+module.exports  = BallContainer;
