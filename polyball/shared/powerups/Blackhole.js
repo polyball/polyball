42a4c2facbeda3229bdbbef16c14cc058750de0a
ryant26 <ryant26@gmail.com>
2016-05-20 11:29:56 -0600

    Blackhole now conforms to new powerup API

--- polyball/shared/powerups/Blackhole.js
+++ polyball/shared/powerups/Blackhole.js
@@ -6,7 +6,6 @@ var Physics = require('physicsjs');
 var inherits = require('inherits');
 var Powerup = require('polyball/shared/powerups/Powerup');
 var StyleCommons = require('polyball/shared/StyleCommons');
-var _ = require('lodash');
 
 // ================================= Private  =================================
 // ============================================================================
@@ -16,11 +15,60 @@ var bhContainer;
 var gameRenderer;
 var Pixi;
 
+// ================================= Public ===================================
+// ============================================================================
+
+/**
+ * This powerup create a gravitational pull in the center of the arena
+ * @param config - See powerup config
+ * @constructor
+ */
+var Blackhole = function(config){
+
+    config.body.styles = StyleCommons.blackholeStyle;
+
+    Powerup.call(this, config);
+    this.name = Blackhole.Name;
+};
+
+inherits(Blackhole, Powerup);
+
+/**
+ * This holds all the logic to activate a blackhole
+ * @param {Model} model
+ */
+Blackhole.prototype.activate = function(model){
+    var arenaCenter = model.arenaContainer.getArena().getCenter();
+    this.attractor = Physics.behavior('attractor', {
+        order: 0,
+        strength: 0.01,
+        pos: arenaCenter
+    });
+
+    model.getWorld().add(this.attractor);
+};
+
+/**
+ * This holds all the logic to deactivate a blackhole
+ * @param {Model} model
+ */
+Blackhole.prototype.deactivate = function (model){
+    model.getWorld().remove(this.attractor);
+};
+
 /**
  * Applies the visual effects for the black hole to the game.
  * @param renderer: Physics.Renderer
  */
-var renderActivate = function(renderer) {
+Blackhole.prototype.renderActivate = function(renderer) {
+    if (Pixi === undefined) {
+        Pixi = renderer.Pixi;
+    }
+
+    if (gameRenderer === undefined) {
+        gameRenderer = renderer;
+    }
+
     var center = renderer.getWorldCenter();
     var clientCenter = renderer.getClientCenter();
     var relCenter = renderer.getRelativePoint(clientCenter);
@@ -33,7 +81,6 @@ var renderActivate = function(renderer) {
     bhContainer.layer = 11;
     bhContainer.addChild(bhSprite);
 
-    // SRS Requirement - 3.2.2.18.7 Black Hole Swell
     twistFilter = new Pixi.filters.TwistFilter();
     twistFilter.radius = 0.08;
     twistFilter.offset.x = relCenter.x;
@@ -57,7 +104,7 @@ var renderActivate = function(renderer) {
  * Removes the visual effects of this black hole from the game.
  * @param renderer: Physics.Renderer
  */
-var renderDeactivate = function(renderer) {
+Blackhole.prototype.renderDeactivate = function(renderer) {
     var newFilters = [];
     for (var i = 0; i < renderer.stage.filters.length; i++) {
         if (renderer.stage.filters[i] !== twistFilter) {
@@ -79,7 +126,7 @@ var renderDeactivate = function(renderer) {
  * Animate the black hole so it looks interesting.
  * @param renderer: Physics.Renderer
  */
-var renderTransform = function(renderer) {
+Blackhole.prototype.renderUpdate = function(renderer) {
     var clientCenter = renderer.getClientCenter();
     var relCenter = renderer.getRelativePoint(clientCenter);
     twistFilter.offset.x = relCenter.x;
@@ -108,91 +155,8 @@ var renderTransform = function(renderer) {
     }
 };
 
-// ================================= Public ===================================
-// ============================================================================
-
-/**
- * This powerup create a gravitational pull in the center of the arena
- * @param config - See powerup config
- * @constructor
- */
-var Blackhole = function(config){
-
-    config.body.styles = StyleCommons.blackholeStyle;
-
-    Powerup.call(this, config);
-    this.name = Blackhole.Name;
-};
-
-inherits(Blackhole, Powerup);
-
-/**
- * This holds all the logic to activate a blackhole
- * @param {Model} model
- */
-// SRS Requirement - 3.2.2.15 Powerup Activated
-// SRS Requirement - 3.2.2.15.3 Blackhole Powerup
-Blackhole.prototype.activate = function(model){
-    if (!this.active) {
-        Blackhole.super_.prototype.activate.call(this, model);
-        var arenaCenter = model.arenaContainer.getArena().getCenter();
-        this.attractor = Physics.behavior('attractor', {
-            order: 0,
-            strength: 0.01,
-            pos: arenaCenter
-        });
-
-        model.getWorld().add(this.attractor);
-
-        this.active = true;
-    }
-};
-
-/**
- * This holds all the logic to deactivate a blackhole
- * @param {Model} model
- */
-Blackhole.prototype.deactivate = function (model){
-    if (this.active){
-        model.getWorld().remove(this.attractor);
-        this.active = false;
-
-        // This is a bit weird here, but it works.
-        if (gameRenderer !== undefined) {
-            renderDeactivate(gameRenderer);
-        }
-    }
-};
-
-Blackhole.prototype.render = function(renderer, model) { //jshint ignore:line
-    if (Pixi === undefined) {
-        Pixi = renderer.Pixi;
-    }
-    if (gameRenderer === undefined) {
-        gameRenderer = renderer;
-    }
-
-    // Activate black hole
-    if (renderer.stage.filters === undefined && this.active) {
-        renderActivate(renderer);
-    }
-    if (renderer.stage.filters !== undefined) {
-        // Activate black hole
-        if (renderer.stage.filters.length === 0 && this.active) {
-            renderActivate(renderer);
-        }
-        // Transform the black hole
-        else if (renderer.stage.filters.length > 0 && this.active) {
-            renderTransform(renderer);
-        }
-    }
-
-};
-
-Blackhole.prototype.toConfig = function (){
-    var config = { name: this.name};
-    _.assign(config, Blackhole.super_.prototype.toConfig.call(this));
-    return config;
+Blackhole.prototype.powerupConfig = function (){
+    return {};
 };
 
 Blackhole.Name = 'Blackhole';
